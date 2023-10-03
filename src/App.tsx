acb08bbe416bfee29c2b984117f77cde581b0357
SinekaRamu <sineka2001cud@gmail.com>
2023-10-03 23:15:59 +0530

    cal

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [num1, getFirstNumber] = useState("");
+  const [num2, getSecondNumber] = useState("");
+  const [result, displayOutput] = useState("0");
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="inputDiv">
+          <label htmlFor="num1">Enter First Number</label>
+          <input
+            type="number"
+            id="num1"
+            onChange={(e) => getFirstNumber(e.target.value)}
+            value={num1}
+          />
+        </div>
+        <div className="inputDiv">
+          <label htmlFor="num2">Enter Second Number</label>
+          <input
+            type="number"
+            id="num2"
+            onChange={(e) => getSecondNumber(e.target.value)}
+            value={num2}
+          />
+        </div>
+        <div></div>
+        <div id="result">{result}</div>
       </header>
     </div>
   );
