10b29f8b3ee4ec47cb470c6e8f4a2b8f31200f0e
SinekaRamu <sineka2001cud@gmail.com>
2024-02-29 21:43:29 +0530

    with add,sub,mul,div

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,48 @@
 import { useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const [num1, getFirstNumber] = useState("");
-  const [num2, getSecondNumber] = useState("");
-  const [result, displayOutput] = useState("0");
+  const [num1, getFirstValue] = useState("");
+  const [num2, getSecondValue] = useState("");
+  const [outres, setOutres] = useState(0);
+
+  const fnHandler = (val: string) => {
+    if (num1 && num2) {
+      const result = eval(num1 + val + num2);
+      if (result) setOutres(result);
+    }
+  };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className="inputDiv">
-          <label htmlFor="num1">Enter First Number</label>
-          <input
-            type="number"
-            id="num1"
-            onChange={(e) => getFirstNumber(e.target.value)}
-            value={num1}
-          />
-        </div>
-        <div className="inputDiv">
-          <label htmlFor="num2">Enter Second Number</label>
-          <input
-            type="number"
-            id="num2"
-            onChange={(e) => getSecondNumber(e.target.value)}
-            value={num2}
-          />
-        </div>
-        <div></div>
-        <div id="result">{result}</div>
-      </header>
-    </div>
+    <header className="App-header">
+      <h1>Simple Calculator</h1>
+      <div className="input-div">
+        <label htmlFor="num1"> Enter First value: </label>
+        <input
+          type="number"
+          id="num1"
+          onChange={(e) => getFirstValue(e.target.value)}
+          value={num1}
+        />
+      </div>
+      <div className="input-div">
+        <label htmlFor="num2">Enter Second value: </label>
+        <input
+          type="number"
+          id="num2"
+          onChange={(e) => getSecondValue(e.target.value)}
+          value={num2}
+        />
+      </div>
+
+      <div>
+        <button onClick={() => fnHandler("+")}>Add</button>
+        <button onClick={() => fnHandler("-")}>Sub</button>
+        <button onClick={() => fnHandler("*")}>Mul</button>
+        <button onClick={() => fnHandler("/")}>Div</button>
+      </div>
+      <div id="result">{outres}</div>
+    </header>
   );
 }
 
